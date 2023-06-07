e72eef7bb155e506d9deab52a6691a874f67c3d9
sayed azp <sayedazp@gmail.com>
2023-06-07 04:11:31 +0300

    task12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 100):
+    print(0)
+    for i in range(1, 100):
         if(i % 15 == 0):
             print("FizzBuzz", end=" ")
         elif(i % 3 == 0):
