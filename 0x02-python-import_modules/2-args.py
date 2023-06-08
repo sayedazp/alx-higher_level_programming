12eee1c7bd3a4190e78bfe8dee94260a5db81ee7
sayed azp <sayedazp@gmail.com>
2023-06-09 01:31:47 +0300

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    len = len(argv)
+
+    if (len == 0):
+        print("0 arguments.")
+    elif (len == 1):
+        print("1 argument.")
+        for idx, x in enumerate(argv):
+            print("{}: {}".format(idx + 1, x))
+    else:
+        print("{} argument.".format(len))
+        for idx, x in enumerate(argv):
+            print("{}: {}".format(idx + 1, x))
