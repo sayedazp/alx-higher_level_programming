fc2db12260c8740c1b3cac8eda147623c2456a42
sayed azp <sayedazp@gmail.com>
2023-06-09 01:40:19 +0300

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    len = len(argv) - 1
+    for i in range(len):
+        add += int(argv[i + 1])
+    print(add)
