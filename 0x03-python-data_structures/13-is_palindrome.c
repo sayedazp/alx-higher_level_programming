fd357d94114866eee798496f2107d513747d9dfb
sayed azp <sayedazp@gmail.com>
2023-06-13 01:23:35 +0300

    task13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a list is a palindrome
+ * @head: pointer to list to be checked
+ * Return: either 1 or 0 determining the state of the list
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *forw;
+	listint_t *backw;
+	int len = 0;
+	int *arr;
+	int i = 0;
+
+	if (head == NULL || (*head) == NULL)
+		return (1);
+	forw = *head;
+	while (forw != NULL)
+	{
+		forw = forw->next;
+		len++;
+	}
+	arr = malloc(sizeof(int) * len);
+	forw = *head;
+	while (forw != NULL)
+	{
+		arr[i] = forw->n;
+		forw = forw->next;
+		i++;
+	}
+	forw = *head;
+	i = 0;
+	while (len - (i + 1) >= i)
+	{
+		if (arr[i] != arr[len - (i + 1)] || len == 2 * i)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
