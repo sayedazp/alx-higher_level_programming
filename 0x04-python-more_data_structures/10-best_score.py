91fe213847cb2a4bced33c26b2c3d415a8b1bc52
sayed azp <sayedazp@gmail.com>
2023-06-15 03:32:19 +0300

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,4 @@ def best_score(a_dictionary):
     biggest = -1
     if a_dictionary is None:
         return None
-    for i in list(a_dictionary):
-        if a_dictionary[i] > biggest:
-            biggest = a_dictionary[i]
-    return biggest if biggest != -1 else None
+    return (max(a_dictionary, key=a_dictionary.get))
