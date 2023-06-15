7812404d61a70f035b9d9b9160f47b558601aba6
sayed azp <sayedazp@gmail.com>
2023-06-15 04:02:15 +0300

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    conv_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                                       'D': 500, 'M': 1000}
+    for i, v in enumerate(roman_string):
+        if i < len(roman_string) - 1:
+            if (conv_dict[roman_string[i]] < conv_dict[roman_string[i + 1]]):
+                sum += -1*conv_dict[v]
+            else:
+                sum += conv_dict[v]
+        else:
+            sum += conv_dict[v]
+    return sum
