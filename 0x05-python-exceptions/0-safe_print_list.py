025e870fac06d2656c15e250cd53ea2ec97d954e
sayed azp <sayedazp@gmail.com>
2023-06-27 02:32:32 +0300

    init

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(listy, x):
     z = 0
     try:
         for y in range(x):
-            print(listy[y], end="")
+            print("{}".format(listy[y]), end="")
             z += 1
         print("")
     except:
