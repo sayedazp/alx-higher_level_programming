58b8bdd258174868e353d298d29508e120e99922
sayed azp <sayedazp@gmail.com>
2023-07-07 15:20:22 +0300

    task6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
             number_of_instances (int): num of recs
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """initialize a new rectangle
             Args:
