8d5708279270c09b86ccd0771063842ef11e3246
sayed azp <sayedazp@gmail.com>
2023-07-22 02:06:37 +0300

    task2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super.__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
