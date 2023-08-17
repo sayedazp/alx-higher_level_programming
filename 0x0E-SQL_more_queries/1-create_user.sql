3654345b6cd365d8d8249a3d7b8cfa2e7e56375e
sayed azp <sayedazp@gmail.com>
2023-08-17 05:27:49 +0300

    task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,7 @@
    -- If the user user_0d_1 already exists, script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- granting previlleges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+--flushing
 FLUSH PRIVILEGES;
