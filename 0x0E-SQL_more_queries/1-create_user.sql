c3ad59e35ceedb317e6c1c58dea4a03bc72563a1
sayed azp <sayedazp@gmail.com>
2023-08-17 05:37:06 +0300

    task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,4 @@
--- creates the MySQL server user user_0d_1.
-   -- user_0d_1 should have all privileges on your MySQL server
-   -- password set to user_0d_1_pwd
-   -- If the user user_0d_1 already exists, script should not fail
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'; -- granting previlleges
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'; -- granting prevs
-
-FLUSH PRIVILEGES; -- flushing
+-- create user: user_0d_1 on localhost with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY
+'user_0d_1_pwd'; 
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
