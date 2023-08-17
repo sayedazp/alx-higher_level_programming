fa0e803e51d9a2e4317134d072a12c7248091c81
sayed azp <sayedazp@gmail.com>
2023-08-17 05:17:17 +0300

    init

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- list user privileges
--- for two users
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
