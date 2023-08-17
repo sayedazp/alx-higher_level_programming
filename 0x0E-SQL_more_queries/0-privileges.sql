cecd86413e021da8b5e92a26eec49c184b32db09
sayed azp <sayedazp@gmail.com>
2023-08-17 05:35:17 +0300

    task1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- script to display privileges of users
 
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
