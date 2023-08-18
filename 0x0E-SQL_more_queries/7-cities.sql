ea89d3b04b8b15900628709fec67e850bf0a09d0
sayed azp <sayedazp@gmail.com>
2023-08-18 04:25:00 +0300

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,17 +1,9 @@
--- creates the database hbtn_0d_usa and the table cities on your MySQL server.
-   -- cities description:
-      -- id INT unique - auto generated, cant be null, is a primary key
-      -- state_id INT - can't be null, must be a FOREIGN KEY that
-      --                references to id of the states table
-      -- name VARCHAR(256) - can't be null
-   -- If the table hbtn_0d_usa already exists, script should not fail
-   -- If the table cities already exists, script should not fail
-
+-- create database, use it, create a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS 
+cities (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       state_id INT NOT NULL,
-       FOREIGN KEY(state_id) REFERENCES states(id),
-       name VARCHAR(256) NOT NULL);
+    name VARCHAR(256) NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
