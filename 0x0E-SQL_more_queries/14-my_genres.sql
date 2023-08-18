0f9048d617e554edcc6d54606f669566bc72865b
sayed azp <sayedazp@gmail.com>
2023-08-18 05:41:14 +0300

    task14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- count and join
-SELECT tv_genres.name as name
-from tv_show_genres INNER JOIN tv_shows
-on tv_show_genres.show_id = tv_shows.id
+SELECT tv_genres.name AS name
+FROM tv_show_genres INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
-on tv_genres.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
