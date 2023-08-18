ca5dff5b0b31107d512bd4ae24acf5563a2aa44a
sayed azp <sayedazp@gmail.com>
2023-08-18 04:49:30 +0300

    task10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- inner join
 SELECT tv_shows.title, tv_show_genres.genre_id
 from tv_show_genres INNER JOIN tv_shows
-on tv_show_genres.show_id = tv_shows.id;
+on tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ;
