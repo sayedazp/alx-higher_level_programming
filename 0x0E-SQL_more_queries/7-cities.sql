4aa69ce16e6c6500ca4c3b6d71abab4f30273bcc
sayed azp <sayedazp@gmail.com>
2023-08-18 04:26:27 +0300

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,5 +6,5 @@ cities (
        id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-        name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
 );
