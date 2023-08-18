c73110241f8a3ca138fd870962aa5f83781ee2c1
sayed azp <sayedazp@gmail.com>
2023-08-18 04:20:20 +0300

    task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS 
 cities (id INT UNIQUE NOT NULL AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL,
-state_id NOT NULL,
+state_id INT NOT NULL,
 PRIMARY KEY (id),
 FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
