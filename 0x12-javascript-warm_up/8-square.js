3ea3429f7e571c1eeafaf95e3e68fc27e6ddcf72
sayedazp <sayedazp@gmail.com>
2023-09-13 21:27:53 +0300

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const x = Number(process.argv[2]);
+
+if (!(isNaN(x))) {
+  for (let i = 0; i < x; i++) {
+    for (let z = 0; z < x; z++) {
+      process.stdout.write('X');
+    }
+    console.log('');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
