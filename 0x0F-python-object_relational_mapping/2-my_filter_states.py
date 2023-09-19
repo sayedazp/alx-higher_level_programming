4375f5a9937aee56d65eb3941308cbe5d6f9f2e4
sayedazp <sayedazp@gmail.com>
2023-09-19 07:36:15 +0300

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""basic sql query using mysqldb"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                 FROM `states` \
+                WHERE `name` = '{}'".format(sys.argv[4]))
+    list = cur.fetchall()
+    for row in list:
+        print(row)
+    cur.close()
+    db.close()
