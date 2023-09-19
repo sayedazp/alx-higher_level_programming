ac7dd10324658717ea11743b7613fb254928d028
sayedazp <sayedazp@gmail.com>
2023-09-19 08:45:52 +0300

    task4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""basic sql query using mysqldb"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+                 FROM `cities` \
+                ORDER BY id")
+    list = cur.fetchall()
+    for row in list:
+        print(row)
+    cur.close()
+    db.close()
