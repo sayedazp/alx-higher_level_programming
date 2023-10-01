a96cb640d13b486bd48a5ea9ce140667161e0f38
sayedazp <sayedazp@gmail.com>
2023-10-01 22:45:18 +0300

    task5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post request with headers
-curl -sL -d "email: test@gmail.com" -d "subject: I will always be here for PLD" -X POST "$1"
+curl -sL -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -X POST "$1"
